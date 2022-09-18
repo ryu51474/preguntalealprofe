213521af1cc5d864c1a9a04eebb30fb40665841c
daniel <ryu51474@gmail.com>
2022-09-18 20:40:00 -0300

    commit con creacion automatica de express

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express=require('express');
+/* const express=require('express');
 const appExpress = express();
 
 const puerto = process.env.PORT||3000;
@@ -7,4 +7,18 @@ appExpress.get('/',(requerimiento,respuesta)=>{
     respuesta.send('pagina principal en linea. para obtener el qr escriba /qr al final en la barra de direcciones');
 });
 
-appExpress.listen(puerto)//,()=>{console.log(`escuchando en http://localhost:${puerto}`)});
+appExpress.listen(puerto)//,()=>{console.log(`escuchando en http://localhost:${puerto}`)}); */
+
+const express = require('express');
+
+    
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+    console.log('Server is running: 3000');
+});
