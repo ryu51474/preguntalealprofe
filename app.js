c8668dbd55fdf8e14ea79dbd8e26cb6e1e9578b0
daniel <ryu51474@gmail.com>
2022-09-18 20:06:26 -0300

    segundo commit con express instalado. a produccion

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const appExpress = express();
+
+const puerto = process.env.PORT||3000;
+
+appExpress.get('/',(requerimiento,respuesta)=>{
+    respuesta.send('pagina principal en linea. para obtener el qr escriba /qr en la barra de direcciones');
+});
+
+appExpress.listen(puerto,()=>{console.log(`escuchando en http://localhost:${puerto}`)});
