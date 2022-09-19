9741663d66886f4f27478936bab39c7f38dc66d5
daniel <ryu51474@gmail.com>
2022-09-18 21:06:23 -0300

    corrijo

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const express = require('express');
-
-    
+const express = require('express');    
 const app = express();
-app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
