efc54d27f94a353a0be94b3ab2576fa6fcb5395e
daniel <ryu51474@gmail.com>
2022-09-18 22:04:15 -0300

    corrigiendo port y vercel. usando render

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const validadorEmail = require('email-validator');
 const { validate, clean, format, getCheckDigit } = require('rut.js');
 const validaRut = (rut)=>{return validate(rut)};
 
+const puerto=process.env.PORT||3000
 appExpress.get('/', (req, res) => {
     res.send('Pagina principal. para encontrar el codigo qr escriba /qr al final en esta barra de direcciones')
 });
@@ -18,7 +19,7 @@ cliente = new Client({
 
   cliente.on("qr", (qr) => {
     console.log("no habia sesion iniciada");
-    console.log(`se inicia sesión, por favor escanee el qr de arriba o visite http://localhost:3000/qr`);
+    console.log(`se inicia sesión, por favor escanee el qr de arriba o visite http://localhost:${puerto}/qr`);
     appExpress.get('/qr',(req,responseweb)=>{
       let qrEnPagina=qrcodeweb.imageSync(qr.toString(),{type:'svg',size:5})
       responseweb.send(qrEnPagina)
@@ -33,6 +34,6 @@ cliente = new Client({
   });
 
 cliente.initialize();
-appExpress.listen(3000, function () {
+appExpress.listen(puerto, function () {
     console.log('Server is running: http://localhost:3000');
 });
