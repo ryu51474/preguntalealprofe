46d35e493586f9337f584383673a7d75a24aa1c1
daniel <ryu51474@gmail.com>
2022-12-13 20:15:00 -0300

    corrijo fecha

--- API_servicios/APIservicios.js
+++ API_servicios/APIservicios.js
@@ -43,7 +43,7 @@ function envioNotas(nombreCompletoUsuario,mensajeUsuario,numeroUsuarioWhatsapp,c
           if (
             respuestaTextodeDireccion != "Estudiante no existe, reintente"
           ) {
-            let nombreArchivomedia = `informe notas de fisica solicitado por ${nombreCompletoUsuario} al ${ahora.getDate()}-${ahora.getMonth()+1}-${ahora.getFullYear()}.html`;
+            let nombreArchivomedia = `informe notas de fisica solicitado por ${nombreCompletoUsuario} al ${ahora.getDate()}_${ahora.getMonth()+1}_${ahora.getFullYear()}.html`;
             let pathFileNombrearchivo = `./informes/${nombreArchivomedia}`;
             //escribo el archivo localmente
             new MessageMedia(//este acto es solo para whatsapp, telegram no lo necesita
