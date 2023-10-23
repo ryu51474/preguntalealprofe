db81ff59a04886f5544736bf03b71d61d5bc0548
ryu51474 <35154561+ryu51474@users.noreply.github.com>
2023-10-23 12:23:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,18 @@ function tokenTlgrm() {
     return tokenProfeDaniel.toString();
 }
 
+function implementacionApiGoogle(){
+    const implementacionApiGoogle = process.env.implementacionApiGoogle // ejemplo 'AKfycbysPjaU_cSu0xPik87OzrmjkZFrTiWoo636w2y6sDMkOouvh0Bn2XBbRT_mGu5_svj2xa';
+    return implementacionApiGoogle.toString()
+}
+
+function numeroAdmin(){
+    const numeroAdmin = process.env.numeroAdmin // ejemplo '56964289006';
+    return numeroAdmin.toString();
+}
+
 module.exports={
-    tokenTlgrm
+    tokenTlgrm,
+    implementacionApiGoogle,
+    numeroAdmin
 }
